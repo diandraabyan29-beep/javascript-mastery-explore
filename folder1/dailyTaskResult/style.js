ed55b653dd35a2eb3cc5501e73bd49206a079a34
diandraabyan <diandraabyan29@gmail.com>
2025-10-30 20:18:00 +0700

    feat: add new

--- folder1/dailyTaskResult/style.js
+++ folder1/dailyTaskResult/style.js
@@ -1,5 +1,13 @@
-function sayHello()
-   { alert('Hello Selamat Datang di potofolio saya');
+function sayHello(){
+    alert('Hello Selamat Datang di potofolio saya');
     // let nama = prompt("Nama Siapa?");
     confirm(" Nama Saya Diandra Abyan Zahran ")
+    
    }
+
+ function resetCount() {
+    console.countReset("submited");
+}
+
+
+
